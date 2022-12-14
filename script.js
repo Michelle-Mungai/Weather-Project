57ca11492d01787fd8244a858a864c3642cef88b
Michelle-Mungai <chellymungai@gmail.com>
2022-12-14 14:11:20 +0300

    Declared variables that are in use from html file

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "1c22fceace8e1677b272701579dc6da4";
+
