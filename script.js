9cf1a6dade49dc2b103d4f4c7626e9410c1477b3
Michelle-Mungai <chellymungai@gmail.com>
2022-12-14 14:17:57 +0300

    Creating the list element to display the city weather data

--- script.js
+++ script.js
@@ -51,4 +51,24 @@ form.addEventListener("submit", e => {
          const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
            weather[0]["icon"]
          }.svg`;
-        })
+         const li = document.createElement("li");
+         li.classList.add("city");
+         const markup = `
+           <h2 class="city-name" data-name="${name},${sys.country}">
+             <span>${name}</span>
+             <sup>${sys.country}</sup>
+           </h2>
+           <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+           <figure>
+             <img class="city-icon" src="${icon}" alt="${
+           weather[0]["description"]
+         }">
+             <figcaption>${weather[0]["description"]}</figcaption>
+           </figure>
+         `;
+         li.innerHTML = markup;
+         list.appendChild(li);
+       })
+       
+  
+   
