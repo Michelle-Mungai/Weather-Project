487239c32d8190801f357c13e1a85c899ada8d83
Michelle-Mungai <chellymungai@gmail.com>
2022-12-14 14:13:05 +0300

    Used eventlistener to get the City when user clicks the submit button

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const list = document.querySelector(".ajax-section .cities");
 
 const apiKey = "1c22fceace8e1677b272701579dc6da4";
 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let inputVal = input.value;
+  
+    //check if there's already a city
+})
