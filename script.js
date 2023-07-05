58915fb7510e3f3fefecf7892a2d2a6cc898830a
Michelle-Mungai <chellymungai@gmail.com>
2023-07-05 19:31:26 +0300

    Removed the login and sign up modals

--- script.js
+++ script.js
@@ -83,19 +83,19 @@ form.addEventListener("submit", e => {
   input.focus();
 });
 // Get the modal
-const modal = document.getElementById('id01');
+// const modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-const model = document.getElementById('id02');
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+// const model = document.getElementById('id02');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == model) {
-        model.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == model) {
+//         model.style.display = "none";
+//     }
+// }
